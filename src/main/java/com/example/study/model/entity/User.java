4dc71b028ebc0bc21e248ac178c0f7d6e5b0216b
JungTag <dbsdudxor121@naver.com>
2021-10-05 00:40:42 +0900

    refactor: JPA Auditing 추가

--- src/main/java/com/example/study/model/entity/User.java
+++ src/main/java/com/example/study/model/entity/User.java
@@ -4,6 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -13,7 +18,8 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@ToString(exclude = {"orderGroup"})
+@ToString(exclude = {"orderGroupList"})
+@EntityListeners(AuditingEntityListener.class)
 //@Table(name = "user") 테이블명 == 클래스명인 경우 생략 가능
 public class User {
 
@@ -27,9 +33,13 @@ public class User {
     private String phoneNumber; // JPA가 Camel Case와 Snake Case를 자동적으로 매핑해줌
     private LocalDateTime registeredAt;
     private LocalDateTime unregisteredAt;
+    @CreatedDate
     private LocalDateTime createdAt;
+    @CreatedBy
     private String createdBy;
+    @LastModifiedDate
     private LocalDateTime updatedAt;
+    @LastModifiedBy
     private String updatedBy;
 
     // User 1 : N OrderGroup
