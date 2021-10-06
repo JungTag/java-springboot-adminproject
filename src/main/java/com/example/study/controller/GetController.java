6bc719c8e51804b213661a5c956a75641da7d0c3
JungTag <dbsdudxor121@naver.com>
2021-10-07 01:10:35 +0900

    feat: REST API와 CRUD 인터페이스 정의

--- src/main/java/com/example/study/controller/GetController.java
+++ src/main/java/com/example/study/controller/GetController.java
@@ -1,6 +1,8 @@
 package com.example.study.controller;
 
 import com.example.study.model.SearchParam;
+import com.example.study.model.network.Header;
+import jdk.javadoc.internal.doclets.formats.html.markup.Head;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,5 +30,11 @@ public class GetController {
         System.out.println(searchParam.getPage());
 
         return searchParam; // jackson 라이브러리를 통해 JSON 형태로 반환
+    }
+
+    @GetMapping("/header")
+    public Header getHeader() {
+        // {"resultCode: "OK", "description" : "OK"}
+        return Header.builder().resultCode("OK").description("OK").build();
     }
 }
